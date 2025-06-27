383fbbecff7ff409aaf554577bc7b7e4cd880516
mahveensultana <167948779+mahveensultana@users.noreply.github.com>
2025-06-27 12:39:41 +0530

    added delivery routes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from app.core.mongodb import lifespan
 from app.users.routes import router as users_router
 from app.users.auth_route import router as auth_router
 from app.delivery.delivery import router as delivery_router
-from app.customer import delivery1_routes
+from app.customer import delivery_routes
 app = FastAPI(lifespan=lifespan)
 
 
@@ -12,7 +12,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth_router, tags=["auth"])
 
 app.include_router(delivery_router)
-app.include_router(delivery1_routes.router)
+app.include_router(delivery_routes.router)
 
 @app.get('/')
 async def main_root() :
