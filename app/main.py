1f692f54dbf415c081936b6c5f260d2628731ed8
N. Harshavardhan <rr200002@rguktrkv.ac.in>
2025-07-02 15:04:46 +0530

    Removing unused routes

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.core.mongodb import lifespan
 from app.users.routes import router as users_router
 from app.users.auth_route import router as auth_router
 from app.delivery.delivery import router as delivery_router
-from app.customer import delivery_routes
+# from app.customer import delivery_routes
 from app.feedback.feedback import router as feedback_router
 app = FastAPI(lifespan=lifespan)
 
@@ -14,7 +14,7 @@ app.include_router(auth_router, tags=["auth"])
 app.include_router(users_router, tags=["CRUD users"])
 
 app.include_router(delivery_router)
-app.include_router(delivery_routes.router)
+# app.include_router(delivery_routes.router)
 
 app.include_router(feedback_router)
 
